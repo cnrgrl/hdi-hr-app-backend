86ce8a004fe51cf37ae04c119bb721b239cb331e
caner <caner@quizco.de>
2022-12-19 03:11:37 +0100

    feat: first api call CREATE success

--- server.js
+++ server.js
@@ -5,4 +5,25 @@ const {db} = require("./firebase.js");
 
 app.use(express.json());
 
+/////// 1-CREATE EMPLOYEE
+app.post("/addemployee", async (req, res) => {
+  const personsRef = db.collection("per").doc();
+  console.log(req.body);
+  await personsRef
+    .set(req.body)
+    .then(() => {
+      res.status(200).send(req.body);
+    })
+    .catch((error) => {
+      console.log("Error adding documents: ", error);
+      res.status(400).send(error);
+    });
+});
+
+/////// 2-READ EMPLOYEES
+
+/////// 3-UPDATE EMPLOYEE
+
+/////// 4-DELETE EMPLOYEE
+
 app.listen(port, () => console.log(`Server has started on port: ${port}`));
